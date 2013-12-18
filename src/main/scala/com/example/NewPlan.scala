482de330f171e53a1040114b7f83b18b25a45d97
Ivan Mikushin <i.mikushin@gmail.com>
2013-12-18 21:04:56 +0600

    new accounts design prototype

--- src/main/scala/com/example/NewPlan.scala
+++ src/main/scala/com/example/NewPlan.scala
@@ -12,29 +12,19 @@ object NewPlan {
 
   val usd = new PaymentAccount {
 
-    def cost(bonesSum: BigDecimal): BigDecimal = ???
-
-  }
-
-  def zeroBalance(usd: BigDecimal): Boolean = ???
-
-  /*
-  object accounts {
-
-    "USD" -> PaymentAccount({a =>
+    def apply(bonesSum: BigDecimal): BigDecimal = {
       val normRate = BigDecimal(0.1)
       val discountRate = BigDecimal(0.05)
       val t = BigDecimal(4)
 
-      a("BonesSUM") match {
+      bonesSum match {
         case x if x <= t => x * normRate
         case x if x > t => t * normRate + (x - t) * discountRate
       }
-    })
+    }
 
   }
-  */
 
-  //val notifications = Set[Notification]()
+  def zeroBalance(usd: BigDecimal): Boolean = notifications.zero(usd)
 
 }
