504c66dc1f32059b005bdde32aa53cc53fe18fa6
Ivan Mikushin <i.mikushin@gmail.com>
2013-12-19 20:06:26 +0600

    new accounts design prototype: aggregate init

--- src/main/scala/com/example/ExamplePlan.scala
+++ src/main/scala/com/example/ExamplePlan.scala
@@ -3,19 +3,26 @@ package com.example
 import kbilling.model._
 
 object ExamplePlan extends BillingPlan {
+
   val accounts = Map(
-    "Bones" -> ServiceAccount(Map("BonesSUM" -> Aggregate())),
+    "Bones" -> ServiceAccount(Map(
+      "SUM" -> Aggregate({_ + _}, {_ => 0})
+    )),
 
     "USD" -> PaymentAccount({a =>
       val normRate = BigDecimal(0.1)
       val discountRate = BigDecimal(0.05)
       val t = BigDecimal(4)
 
-      a("BonesSUM") match {
+      a("Bones.SUM") match {
         case x if x <= t => x * normRate
         case x if x > t => t * normRate + (x - t) * discountRate
       }
     })
   )
-  val notifications = Set[Notification]()
+
+  val notifications = Set[Notification](
+    Notification(a => a("USD") == 0, "zeroBalance")
+  )
+
 }
